e83ddfc34da76c87819220a7a65dc76192b8cea7
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2018-06-23 22:39:28 -0500

    Demostración de pase de propiedades a un componente de React.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { Grid } from "react-bootstrap";
 
 export class App extends Component {
   render() {
-    return <div>Hola React!</div>;
+    const { nombre, mensaje } = this.props;
+    return (
+      <Grid>
+        <h1>Hola {nombre}!</h1>
+        <p>{mensaje}</p>
+      </Grid>
+    );
   }
 }
+
+App.defaultProps = {
+  nombre: "React",
+  mensaje: "Mensaje por default"
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
+ReactDOM.render(
+  <App nombre="Edgar" mensaje="Este es un mensaje desde los parámetros." />,
+  document.getElementById("root")
+);
