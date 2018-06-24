9a0585cc96d4dfafd66c42b7cb8f8dc4c6070b6e
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2018-06-23 23:36:01 -0500

    Demostración de Eventos de Usuario y llamado a funciones.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
-import { Grid } from "react-bootstrap";
+import {
+  Grid,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Button
+} from "react-bootstrap";
 
 export class App extends Component {
+  state = {
+    nombre: this.props.nombre
+  };
+
+  alOprimirBoton = e => {
+    e.preventDefault();
+    this.setState({
+      nombre: e.target.nombre.value
+    });
+  };
+
   render() {
-    const { nombre, mensaje } = this.props;
+    const { nombre } = this.state;
+    const { mensaje } = this.props;
     return (
       <Grid>
         <h1>Hola {nombre}!</h1>
         <p>{mensaje}</p>
+        <form onSubmit={this.alOprimirBoton}>
+          <FormGroup>
+            <ControlLabel>Nombre</ControlLabel>
+            <FormControl type="text" id="nombre" name="nombre" />
+            <Button type="submit">Saludar</Button>
+          </FormGroup>
+        </form>
       </Grid>
     );
   }
